ba0ff55dd430f2db05808199ee326874c44c3680
sofia_pohuliailo <spohuliailo@gmail.com>
2026-08-18 19:13:48 +0300

    Solution changes

--- app/test_main.py
+++ app/test_main.py
@@ -3,16 +3,17 @@ from app.main import can_access_google_page
 
 
 def test_should_access_google_page_when_url_valid_and_has_internet() -> None:
-    with patch("app.main.valid_google_url", return_value=True,), patch("app.main.has_internet_connection", return_value=True,):
+    with (patch("app.main.valid_google_url", return_value=True,),
+          patch("app.main.has_internet_connection", return_value=True,)):
         assert can_access_google_page("some_url") == "Accessible"
 
 
 def test_should_access_google_page_when_url_valid_and_no_internet() -> None:
-    with patch("app.main.valid_google_url", return_value=True,), patch("app.main.has_internet_connection", return_value=False,):
+    with (patch("app.main.valid_google_url", return_value=True,),
+          patch("app.main.has_internet_connection", return_value=False,)):
         assert can_access_google_page("some_url") == "Not accessible"
 
 
 def test_should_access_google_page_when_url_invalid() -> None:
     with patch("app.main.valid_google_url", return_value=False,):
         assert can_access_google_page("some_url") == "Not accessible"
-
